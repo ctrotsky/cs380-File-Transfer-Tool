4a74b15f677210c3495305a12f157d987ccf41c5
rockyroad1228 <rockyroad1228@gmail.com>
2016-11-28 15:38:21 -0800

    driver up

--- CS380_FileTransfer/src/filetransfer/Driver.java
+++ CS380_FileTransfer/src/filetransfer/Driver.java
@@ -1,50 +1,98 @@
 package filetransfer;
 
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Driver {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-//		//Leave uncommented on computer that will send file.
-//		Client sendClient = new Client();
-//		sendClient.setFilePath("E:/Documents/SocketTesting/FileClient1/SendFile.txt");
-//		sendClient.setKeyFile("E:/Documents/SocketTesting/FileClient1/keyfile.txt");
-//		sendClient.setTargetIP("192.168.1.13");
-//		sendClient.setSocketPort(13267);
-//		sendClient.setPacketSize(8000);
-//		sendClient.sendFile();
 //		
+		Client c = new Client();
+		String filePath;
+		String keyPath;
+		String username;
+		String password;
+		String ip;
+		int port=22;
+		int as=0;
+		boolean ascii;
+		int sr=0;
 		
-//		//Leave uncommented on computer that will receive file.
-//		Client receiveClient = new Client();
-//		receiveClient.setFilePath("C:/Users/Colin/Documents/FileTransfer/Client2/RockyFile.avi");
-//		receiveClient.setKeyFile("C:/Users/Colin/Documents/FileTransfer/Client2/C00130.pdf");
-//		receiveClient.setTargetIP("10.110.35.113");
-//		receiveClient.setSocketPort(22);
-//		receiveClient.setPacketSize(5000);
-//		receiveClient.receiveFile();
-//		
-		ThreadRunner sendClient = new ThreadRunner();
-		sendClient.setFilePath("C:/Users/Colin/Documents/FileTransfer/Client1/Jet1.mp4");
-		sendClient.setKeyFile("C:/Users/Colin/Documents/FileTransfer/Client1/keyfile.txt");
-		sendClient.setTargetIP("10.110.123.221");
-		sendClient.setSocketPort(13267);
-		sendClient.setPacketSize(40);
-		sendClient.setToSend();
-		sendClient.setAsciiArmored(true);
+		Scanner scan=new Scanner(System.in);
 		
-		ThreadRunner receiveClient = new ThreadRunner();
-		receiveClient.setFilePath("C:/Users/Colin/Documents/FileTransfer/Client2/Jet1.mp4");
-		receiveClient.setKeyFile("C:/Users/Colin/Documents/FileTransfer/Client2/keyfile.txt");
-		receiveClient.setTargetIP("10.110.123.221");
-		receiveClient.setSocketPort(13267);
-		receiveClient.setPacketSize(40);
-		receiveClient.setToReceive();
-		receiveClient.setAsciiArmored(true);
+		System.out.print("enter 1 for recieve client, 2 for send: ");
+		sr= scan.nextInt();
+		scan.nextLine();
+		System.out.print("Enter Target File Path for file: ");
+		filePath= scan.nextLine();
+		System.out.print("Enter file path for key: ");
+		keyPath=scan.nextLine();
+		System.out.print("Enter IP of other client: ");
+		ip=scan.nextLine();
+		System.out.print("enter 1 for Ascii Encode, any # for no: ");
+		as=scan.nextInt();
+		if(as==1)
+			ascii=true;
+		else 
+			ascii=false;
+		if (sr==1)
+		{
 		
-		Thread sendThread = new Thread(sendClient);
-		Thread receiveThread = new Thread(receiveClient);
-		sendThread.start();
-		receiveThread.start();
+			c.setFilePath(filePath);
+			c.setKeyFile(keyPath);
+			c.setTargetIP(ip);
+			c.setSocketPort(port);
+			c.setAsciiArmored(ascii);
+			c.receiveFile();
+			
+			
+		}
+		
+		else if(sr==2)
+		{
+			scan.nextLine();
+			System.out.print("Enter Username: ");
+			username=scan.nextLine();
+			System.out.print("Enter Password: ");
+			password=scan.nextLine();
+			c.setFilePath(filePath);
+			c.setKeyFile(keyPath);
+			c.setTargetIP(ip);
+			c.setSocketPort(port);
+			c.setAsciiArmored(ascii);
+			c.sendFile();
+			
+		}
+		
+		else 
+		{
+			System.out.println("not a valid input");
+		}
+		
+		
+		
+		
+		
+		
+		
+		//Leave uncommented on computer that will send file.
+		/*Client sendClient = new Client();
+		sendClient.setFilePath("C:/cs380/sample.avi");
+		sendClient.setKeyFile("2");
+		sendClient.setTargetIP("192.168.45.128");
+		sendClient.setSocketPort(22);
+		sendClient.setPacketSize(50000);
+		sendClient.setAsciiArmored(false);
+		sendClient.sendFile();
+		*/
+		//Leave uncommented on computer that will receive file.
+		//Client receiveClient = new Client();
+		//receiveClient.setFilePath("C:/cs380/sample.avi");
+		//receiveClient.setKeyFile("C:/cs380/C00130.pdf");
+		//receiveClient.setTargetIP("192.168.1.6");
+		//receiveClient.setSocketPort(13267);
+		//receiveClient.setPacketSize(55000);
+		//receiveClient.receiveFile();
 	}
 }
